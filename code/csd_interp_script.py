3ec4077d87da9d763cb58640711e58be07f6f95a
Nick Tolley <nic.m.tolley@gmail.com>
2020-05-06 10:48:35 -0400

    Add scripts to convert raw LFP output to interpolated CSD points

---
+++ code/csd_interp_script.py
@@ -0,0 +1,22 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import seaborn as sns
+import csd_functions
+import os
+import sys
+
+file_prefix = 'gbarEvPyrAmpa_sweep'
+hnn_dir = os.path.abspath('template_hnn_github/data/' + file_prefix + '/hnn_out/data')
+
+os.mkdir('data/' + file_prefix)
+save_dir = os.path.abspath('data/' + file_prefix) #directory to store param files
+
+for idx, f in enumerate(os.listdir(hnn_dir)):
+    data_path = hnn_dir + '/' +  f + '/'
+    save_path = save_dir + '/' + f + '.csv'
+    Z_hnn = csd_functions.csd_interp(data_path,20)
+    csd_points = csd_functions.grid2points(Z_hnn)
+    np.savetxt(save_path, csd_points, delimiter=',')
+
